aa65c76441f33828f433d27c2d3ad4ace367efe8
IamVarya <wioleta.jakacka@gmail.com>
2022-02-17 14:46:59 +0100

    dodano zmiane tekstu, zmiane koloru, zmiane font-size

---
+++ Java/java.js
@@ -0,0 +1,8 @@
+function changeColor(){
+    if (document.getElementById("myDiv").style.backgroundColor == "rgb(144, 5, 5)") {
+        document.getElementById("myDiv").style.backgroundColor = "rgb(12, 141, 29)";
+    } else {
+        document.getElementById("myDiv").style.background = "rgb(144, 5, 5)";
+    }
+}
+
