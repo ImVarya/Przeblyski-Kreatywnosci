b56b1f410f683044b3ea9ed039fc8232bc33b7b9
IamVarya <wioleta.jakacka@gmail.com>
2022-02-17 16:58:35 +0100

    naprawiony bug (klikanie 2 razy, zanim zadzialalo)

--- Java/java.js
+++ Java/java.js
@@ -1,26 +1,26 @@
 function changeColor() {
-    if (document.getElementById("myDiv").style.backgroundColor == "rgb(144, 5, 5)") {
-        document.getElementById("myDiv").style.backgroundColor = "rgb(12, 141, 29)";
+    if (document.getElementById("myDiv").style.backgroundColor == "rgb(12, 141, 29)") {
+        document.getElementById("myDiv").style.backgroundColor = "rgb(144, 5, 5)";
     } else {
-        document.getElementById("myDiv").style.background = "rgb(144, 5, 5)";
+        document.getElementById("myDiv").style.background = "rgb(12, 141, 29)";
     }
 }
 
 function changeText() {
     var id = document.getElementsByName("button")[0].id;
     if(id == 1) {
-        document.getElementById("demo").innerHTML = "Zmiana tekstu przyciskiem";
+        document.getElementById("demo").innerHTML = "Zmieniony tekst";
         document.getElementsByName("button")[0].id = 0;
     } else {
-        document.getElementById("demo").innerHTML = "Zmieniony tekst";
+        document.getElementById("demo").innerHTML = "Zmiana tekstu przyciskiem";
         document.getElementsByName("button")[0].id = 1;
     }
 }
 
 function changeFontsize() {
-    if (document.getElementById ("par").style.fontSize == "30px") {
-        document.getElementById("par").style.fontSize = "50px";
-    } else {
+    if (document.getElementById ("par").style.fontSize == "50px") {
         document.getElementById("par").style.fontSize = "30px";
+    } else {
+        document.getElementById("par").style.fontSize = "50px";
     }
 }
