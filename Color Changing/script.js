a6003b97e4dce021c8c66b939a873b0c2b6bc111
IamVarya <wioleta.jakacka@gmail.com>
2022-03-02 01:18:36 +0100

    dodano kolejna funkcje, button oraz input, ktory pozwala wybrac kolor z color pickera, div zmienia kolor

--- Color Changing/script.js
+++ Color Changing/script.js
@@ -2,3 +2,7 @@ function colorChange() {
     let kolor = document.getElementById("colorInput").value;
     document.getElementById("color").style.backgroundColor = kolor;
 }
+function pickColor() {
+    let kolor = document.getElementById("colorPicker").value;
+    document.getElementById("color").style.backgroundColor = kolor;
+}
