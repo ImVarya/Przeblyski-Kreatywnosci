ce68e8b3d8b5ac480fbfc239e0c5eab76dcc9b9e
IamVarya <wioleta.jakacka@gmail.com>
2022-04-25 23:22:34 +0200

    added some code

---
+++ script.js
@@ -0,0 +1,29 @@
+let x = "";
+
+
+for (i = 0; i < 5; i++) {
+    x = "*" + x;
+    console.log(x);
+}
+
+
+// function triangle2 (z) {
+//   for (var i = 1; i <= z; i++) {
+//     var y = "";
+//     for (var j = 1; j <= (z - i + 1); j++) {
+//       y += "*";
+//     }
+//     console.log(y);
+//   }
+// }
+
+// triangle2(4);
+
+  for (i = 4; i > 0; i--) {
+    c = "*";
+    console.log(c.repeat(i));
+  }
+
+// for (let i = "*"; i.length <= 5; i += "*") {
+//   console.log(i);
+// }
