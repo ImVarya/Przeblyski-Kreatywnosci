0f1eefee0d9916340a629f6028d27bf5651190af
IamVarya <wioleta.jakacka@gmail.com>
2022-05-05 14:31:29 +0200

    finished version of Flappy Bird, dodano pipes, liczenie pipes oraz c

--- flappyBird/script.js
+++ flappyBird/script.js
@@ -1,4 +1,5 @@
 import { updateBird, setupBird, getBirdRect} from "./bird.js"
+import { updatePipes, setupPipes, getPassedPipeCount, getPipeRects } from "./pipe.js"
 
 document.addEventListener("keypress", handleStart, { once: true })
 const title = document.querySelector("[data-title]")
@@ -13,6 +14,7 @@ function updateLoop(time) {
     }
 const delta = time - lastTime
 updateBird(delta)
+updatePipes(delta)
 if (checkLose()) return handleLose()
 lastTime = time
 window.requestAnimationFrame(updateLoop)
@@ -20,13 +22,24 @@ window.requestAnimationFrame(updateLoop)
 
 function checkLose() {
     const birdRect = getBirdRect()
+    const insidePipe = getPipeRects().some(rect => isCollision(birdRect, rect))
     const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
-    return outsideWorld
+    return outsideWorld || insidePipe
+}
+
+function isCollision(rect1, rect2) {
+    return (
+        rect1.left < rect2.right &&
+        rect1.top < rect2.bottom &&
+        rect1.right > rect2.left &&
+        rect1.bottom > rect2.top
+    )
 }
 
 function handleStart() {
     title.classList.add("hide")
     setupBird()
+    setupPipes()
     lastTime = null
     window.requestAnimationFrame(updateLoop)
 }
@@ -35,7 +48,7 @@ function handleLose() {
     setTimeout(() => {
         title.classList.remove("hide")
         subtile.classList.remove("hide")
-        subtile.textContent = "0 Pipes"
+        subtile.textContent = `${getPassedPipeCount()} Pipes`
         document.addEventListener("keypress", handleStart, { once: true })
     }, 100)
 }
