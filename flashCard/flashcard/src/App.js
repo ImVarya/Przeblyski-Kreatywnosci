253e1b6c2a7b940810abfe2887bd9285c18fbefc
IamVarya <wioleta.jakacka@gmail.com>
2022-07-03 02:03:36 +0200

    dodano komponenty oraz dodawanie pytan

--- flashCard/flashcard/src/App.js
+++ flashCard/flashcard/src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import FlashCard from "./FlashCard";
+import FlashList from "./FlashList";
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div>
-      yo i like you
-    </div>
+    <FlashList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2?",
+    answer: "4",
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+
+  {
+    id: 2,
+    question: "Question 2?",
+    answer: "Answer",
+    options: [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4"
+    ]
+  }
+]
 
 export default App;
