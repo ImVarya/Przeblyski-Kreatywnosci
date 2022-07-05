655825f03164315329752ceb61a61b9fb047c7b1
IamVarya <wioleta.jakacka@gmail.com>
2022-07-05 23:39:10 +0200

    dodano resizowanie dynamicznie wysokosci flashcard

--- flashCard/flashcard/src/FlashCard.js
+++ flashCard/flashcard/src/FlashCard.js
@@ -2,13 +2,30 @@ import React, { useState, useEffect, useRef } from 'react'
 
 export default function FlashCard({ flashcard }) {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backtHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backtHeight, 100))
+  }
+
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setHeight)
+  }, [])
 
   return (
     <div
       className={`card ${flip ? 'flip' : ""}`}
+      style={{ height: height }}
       onClick={() => setFlip(!flip)}
     >
-      <div className='front'>
+      <div className='front' ref={frontEl}>
         {flashcard.question}
         <div className='flashcard-options'>
           {flashcard.options.map(option => {
@@ -16,7 +33,7 @@ export default function FlashCard({ flashcard }) {
           })}
         </div>
       </div>
-      <div className='back'>{flashcard.answer}</div>
+      <div className='back' ref={backEl}>{flashcard.answer}</div>
     </div>
   )
 }
