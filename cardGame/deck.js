b02bd537f5c85f4909caf19b84ab280247f3c141
IamVarya <wioleta.jakacka@gmail.com>
2022-07-15 20:46:17 +0200

    dodano renderowanie karty

--- cardGame/deck.js
+++ cardGame/deck.js
@@ -38,7 +38,18 @@ class Card {
     constructor(suit, value) {
         this.suit = suit
         this.value = value
-        "nah"
+    }
+
+    get color() {
+        return this.suit === "♠" || this.suit === "♣" ? "black" : "red"
+    }
+
+    getHTML() {
+        const cardDiv = document.createElement("div")
+        cardDiv.innerText = this.suit
+        cardDiv.classList.add("card", this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}`
+        return cardDiv
     }
 }
 
